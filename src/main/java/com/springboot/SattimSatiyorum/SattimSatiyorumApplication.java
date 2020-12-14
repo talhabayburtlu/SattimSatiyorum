53ad482f6a639ff13ad89ff9d1a6256155f207cc
talhabayburtlu <talha_bayburtlu2@hotmail.com>
2020-12-14 15:28:33 +0300

    Init commit.

---
+++ src/main/java/com/springboot/SattimSatiyorum/SattimSatiyorumApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.SattimSatiyorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SattimSatiyorumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SattimSatiyorumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/SattimSatiyorum/SattimSatiyorumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.SattimSatiyorum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SattimSatiyorumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
